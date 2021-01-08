b60096e2d71907eaddd4421ef304eff3cc794b9e
Parisa Nikzad <parisa.nikzad@yahoo.com>
2021-01-07 19:28:00 -0500

    BCrypt password

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Spring-Seurity
 - The first step to bring security to the spring project is adding " spring boot security". This will add a login page that take "user" as username, and the password is provided in the console when your Spring Boot project is starting. 
 - To be able to add the proper username and password, we need to add configuration class that inherits from WebSecurityConfigurerAdapter. As this is a configuration class we need to add:  
-    @Configuration 
-    and 
-    @EnableWebSecurity
-    there are two ways to do that:
-    - override UserDetailsService.
+        @Configuration <br />
+        @EnableWebSecurity<br />
+    There are two ways to provides user and password:
+    - override UserDetailsService, which requires using withDefaultPasswordEncoder() that is a deprecated method, and you will be exposing the password.
+    - using PasswordEncoder which BCrypt the password.
+    <br />for demo purpose I kept both code.
     
--- src/main/java/com/pn/secureApp/configuration.java
+++ src/main/java/com/pn/secureApp/configuration.java
@@ -2,11 +2,14 @@ package com.pn.secureApp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,12 +19,26 @@ import java.util.List;
 @EnableWebSecurity
 public class configuration extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2";
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder().username("Parisa").password("1234").roles("ADMIN").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
+
     @Override
-    protected UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder().username("Parisa").password("1234").roles("ADMIN").build());
-        return new InMemoryUserDetailsManager(users);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("Parisa").password(ENCODED_PASSWORD).roles("ADMIN");
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
